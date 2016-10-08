0405cf3f6a4dd2243f2f01de351497611c794f9c
Chris Kim <chriskim06@gmail.com>
2016-10-08 13:17:20 -0700

    Adding default notebook on startup if none exist

--- models/db.js
+++ models/db.js
@@ -4,7 +4,18 @@
  */
 
 import redis from 'redis'
+import Notebook from './Notebook'
 
 const client = redis.createClient()
+
+client.hgetall('notebooks', (err, reply) => {
+  if (!reply || !Object.keys(reply).length) {
+    Notebook.persist(new Notebook('default'), (err, res) => {
+      if (err) {
+        console.error(err)
+      }
+    })
+  }
+})
 
 export default client
